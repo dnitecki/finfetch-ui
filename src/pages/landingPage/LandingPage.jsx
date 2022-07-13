cf9dea96bee657e0c989b779c828a9a341d78f9d
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-13 18:02:05 -0500

    landing page styles

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import "./LandingPage.scss";
 import { NavLink } from "react-router-dom";
 
 export default function LandingPage() {
   return (
-    <div>
-      <div>Landing Page</div>
-      <NavLink to="dashboard">Dashboard</NavLink>&nbsp;
-      <NavLink to="api">API</NavLink>
+    <div className="landingPage">
+      <div className="landingPage-container">
+        <NavLink to="dashboard">Dashboard</NavLink>&nbsp;&nbsp;
+        <NavLink to="api">API</NavLink>
+      </div>
     </div>
   );
 }
