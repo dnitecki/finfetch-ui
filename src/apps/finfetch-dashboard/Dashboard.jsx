4bd5a4b027747bc99005bf5bd2b867550a59f9a7
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-13 15:50:31 -0500

    add icons

--- src/apps/finfetch-dashboard/Dashboard.jsx
+++ src/apps/finfetch-dashboard/Dashboard.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Dashboard.scss";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Overview from "./pages/overview/Overview";
 import WatchList from "./pages/watchlist/WatchList";
 import ReportBug from "./pages/reportbug/ReportBug";
 import Sidebar from "./components/sidebar/Sidebar";
+import "primeicons/primeicons.css"; //icons
 
 export default function Dashboard() {
+  const location = useLocation();
   return (
     <div className="app-dashboard">
       <div className="app-dashboard-container">
         <div className="dashboard-sidebar">
           <Sidebar />
         </div>
-        <Routes>
+        <Routes location={location} key={location.pathname}>
           <Route path="overview" element={<Overview />} />
           <Route path="watchlist" element={<WatchList />} />
           <Route path="reportbug" element={<ReportBug />} />
