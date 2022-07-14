86d6abb838701994d7c68ab8dcc8c8e57462ed84
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-14 09:39:52 -0500

    landing page css

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -6,8 +6,14 @@ export default function LandingPage() {
   return (
     <div className="landingPage">
       <div className="landingPage-container">
-        <NavLink to="dashboard">Dashboard</NavLink>&nbsp;&nbsp;
-        <NavLink to="api">API</NavLink>
+        <div className="landingPage-apps">
+          <NavLink to="dashboard/overview">
+            <div className="landingPage-card">Dashboard </div>
+          </NavLink>
+          <NavLink to="api">
+            <div className="landingPage-card">API </div>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
