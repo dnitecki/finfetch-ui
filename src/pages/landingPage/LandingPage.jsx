6e226473b326614904fba05589880db308fde203
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-14 15:03:03 -0500

    background css

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -2,19 +2,37 @@ import React from "react";
 import "./LandingPage.scss";
 import { NavLink } from "react-router-dom";
 
+const changeBgDashboard = () => {
+  document.getElementById("landingPage").style.background =
+    "rgba(9, 32, 63, 1)";
+};
+const changeBgApi = () => {
+  document.getElementById("landingPage").style.background = "green";
+};
+const resetBg = () => {
+  document.getElementById("landingPage").style.background = "#91a3b0";
+};
 export default function LandingPage() {
   return (
-    <div className="landingPage">
+    <div id="landingPage" className="landingPage">
       <div className="landingPage-container">
         <div className="landingPage-text">
-          <img className="landingPage-icon" src="./assets/finfetch-icon.png" />
+          <img
+            className="landingPage-icon"
+            src="./assets/finfetch-icon.png"
+            alt="FinFetch.io"
+          />
           <div className="landingPage-name">FinFetch.io</div>
         </div>
         <div className="landingPage-apps">
-          <NavLink to="dashboard/overview">
+          <NavLink
+            to="dashboard/overview"
+            onMouseOver={changeBgDashboard}
+            onMouseLeave={resetBg}
+          >
             <div className="landingPage-card">Dashboard </div>
           </NavLink>
-          <NavLink to="api">
+          <NavLink to="api" onMouseOver={changeBgApi} onMouseLeave={resetBg}>
             <div className="landingPage-card">API </div>
           </NavLink>
         </div>
