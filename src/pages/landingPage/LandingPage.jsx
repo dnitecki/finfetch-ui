09fe8a5400fd36b36ad0df9b515b43979ba55d50
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-14 19:39:03 -0500

    landing page styles

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -3,42 +3,61 @@ import "./LandingPage.scss";
 import { NavLink } from "react-router-dom";
 
 const changeBgDashboard = () => {
-  document.getElementById("landingPage").style.background =
-    "rgba(9, 32, 63, 1)";
-  document.getElementById("landingPage-name").style.color = "white";
+  document.getElementById("landingPage").style.background = "#20124E";
+  document.getElementById("landingPage-main").style.display = "none";
+  document.getElementById("landingPage-api").style.display = "none";
+  document.getElementById("landingPage-dashboard").style.display = "flex";
 };
 const changeBgApi = () => {
-  document.getElementById("landingPage").style.background = "green";
+  document.getElementById("landingPage").style.background = "#2170FA";
+  document.getElementById("landingPage-main").style.display = "none";
+  document.getElementById("landingPage-dashboard").style.display = "none";
+  document.getElementById("landingPage-api").style.display = "flex";
 };
 const resetBg = () => {
-  document.getElementById("landingPage").style.background = "#91a3b0";
-  document.getElementById("landingPage-name").style.color = "black";
+  document.getElementById("landingPage").style.background = "#e9e9e9";
+  document.getElementById("landingPage-api").style.display = "none";
+  document.getElementById("landingPage-dashboard").style.display = "none";
+  document.getElementById("landingPage-main").style.display = "flex";
 };
 export default function LandingPage() {
   return (
     <div id="landingPage" className="landingPage">
       <div className="landingPage-container">
         <div className="landingPage-text">
-          <img
-            className="landingPage-icon"
-            src="./assets/finfetch-icon.png"
-            alt="FinFetch.io"
-          />
-          <div id="landingPage-name" className="landingPage-name">
-            FinFetch.io
+          <div id="landingPage-main">
+            <img
+              className="landingPage-icon"
+              src="./assets/landingPage-image.png"
+              alt="FinFetch.io"
+            />
+          </div>
+          <div id="landingPage-dashboard">
+            <img
+              className="landingPage-icon"
+              src="./assets/dashboard-image.png"
+              alt="FinFetch.io"
+            />
+          </div>
+          <div id="landingPage-api">
+            <img
+              className="landingPage-icon"
+              src="./assets/api-image.png"
+              alt="FinFetch.io"
+            />
           </div>
         </div>
         <div className="landingPage-apps">
           <NavLink
             to="dashboard/overview"
             onMouseOver={changeBgDashboard}
-            onMouseLeave={resetBg}
+            // onMouseLeave={resetBg}
           >
             <div className="landingPage-card">
               <div className="landingPage-card-content">Dashboard</div>
             </div>
           </NavLink>
-          <NavLink to="api" onMouseOver={changeBgApi} onMouseLeave={resetBg}>
+          <NavLink to="api" onMouseOver={changeBgApi}>
             <div className="landingPage-card">
               <div className="landingPage-card-content">API</div>
             </div>
