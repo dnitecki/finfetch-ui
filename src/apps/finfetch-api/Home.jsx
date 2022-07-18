abd96d5a35065880b52f7912214268425cf762b7
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-17 20:38:37 -0500

    add login and register pages

--- src/apps/finfetch-api/Home.jsx
+++ src/apps/finfetch-api/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Home.scss";
 import Navbar from "./components/navbar/Navbar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { Route, Routes, useLocation } from "react-router-dom";
 
 export default function Home() {
+  const location = useLocation();
   return (
     <div className="app-api">
       <div className="app-api-container">
         <Navbar />
+        <Routes location={location} key={location.pathname}>
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
       </div>
     </div>
   );
