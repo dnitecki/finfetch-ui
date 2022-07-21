6b0f15484f52966567f53bceb957bca2817138fd
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-21 11:04:55 -0500

    login form

--- src/apps/finfetch-api/pages/login/Login.jsx
+++ src/apps/finfetch-api/pages/login/Login.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import "./Login.scss";
+import icon from "../../../../assets/FinFetch-icon-small.png";
 
 export default function Login() {
   return (
     <div className="login">
-      <div>Login</div>
+      <div className="login-container">
+        <div className="login-header">
+          <img className="login-icon" src={icon} alt="FinFetch.io" />
+          <div className="login-header-text">Sign In</div>
+        </div>
+        <div className="login-form">
+          <label className="form-label" htmlFor="email">
+            Email:
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            id="email"
+            autoComplete="off"
+            required
+          />
+          <label className="form-label" htmlFor="password">
+            Password:
+          </label>
+          <input
+            className="form-input"
+            type="password"
+            id="password"
+            required
+          />
+          <button
+            className="form-button"
+            // disabled={!validEmail || !validPwd || !validMatch ? true : false}
+          >
+            Sign In
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
