e7b5174d7051e41a7d10c2df86272a55ca257078
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-07-22 20:37:13 -0500

    syntax highlighting

--- src/apps/finfetch-api/pages/documentation/Documentation.jsx
+++ src/apps/finfetch-api/pages/documentation/Documentation.jsx
@@ -1,14 +1,52 @@
 import React from "react";
 import "./Documentation.scss";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { coy } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function Documentation() {
   return (
     <div className="documentation">
       <div className="documentation-container">
         <div className="documentation-sidebar"></div>
-        <div className="documentation-content">
+        <div className="documentation-content-container">
           <div className="documentation-header"></div>
           <div className="documentation-content-header">API Documentation</div>
+          <div className="documentation-content">
+            <div className="documentation-text">Sample Api Call</div>
+            <div className="documentation-code">
+              <SyntaxHighlighter
+                className="syntax-highlighter"
+                language="json"
+                style={coy}
+              >
+                'akjfakjfhkajfhak'
+              </SyntaxHighlighter>
+            </div>
+          </div>
+          <div className="documentation-content">
+            <div className="documentation-text">Sample Api Call</div>
+            <div className="documentation-code">
+              <SyntaxHighlighter
+                className="syntax-highlighter"
+                language="javascript"
+                style={coy}
+              >
+                'akjfakjfhkajfhak'
+              </SyntaxHighlighter>
+            </div>
+          </div>
+          <div className="documentation-content">
+            <div className="documentation-text">Sample Api Call</div>
+            <div className="documentation-code">
+              <SyntaxHighlighter
+                className="syntax-highlighter"
+                language="javascript"
+                style={coy}
+              >
+                'akjfakjfhkajfhak'
+              </SyntaxHighlighter>
+            </div>
+          </div>
         </div>
       </div>
     </div>
