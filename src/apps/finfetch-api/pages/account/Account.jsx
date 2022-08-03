185c21d38f725e55cdebfbbb1190cf535da46925
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-02 21:19:25 -0500

    account styles

--- src/apps/finfetch-api/pages/account/Account.jsx
+++ src/apps/finfetch-api/pages/account/Account.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./Account.scss";
 
 export default function Account() {
-  return <div>Account</div>;
+  return (
+    <div className="account">
+      <div className="account-container">
+        <div className="account-header"></div>
+      </div>
+    </div>
+  );
 }
