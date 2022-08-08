1d476e9a1e9a958ea72ff03dd41eeff29e8605a4
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-08 15:10:31 -0500

    loginStatus context place level higher

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import "./app.scss";
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
 import Dashboard from "./apps/finfetch-dashboard/Dashboard";
 import Home from "./apps/finfetch-api/Home";
+import UserContext from "./components/context/UserContext";
 
 function App() {
+  const [loginStatus, setLoginStatus] = useState(false);
   return (
     <div>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/api/*" element={<Home />} />
-        <Route exact path="/dashboard/*" element={<Dashboard />} />
-      </Routes>
+      <UserContext.Provider value={{ loginStatus, setLoginStatus }}>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/api/*" element={<Home />} />
+          <Route exact path="/dashboard/*" element={<Dashboard />} />
+        </Routes>
+      </UserContext.Provider>
     </div>
   );
 }
--- src/apps/finfetch-api/Home.jsx
+++ src/apps/finfetch-api/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import "./Home.scss";
 import Navbar from "./components/navbar/Navbar";
 import Login from "./pages/login/Login";
@@ -7,23 +6,19 @@ import Documentation from "./pages/documentation/Documentation";
 import Register from "./pages/register/Register";
 import Account from "./pages/account/Account";
 import { Route, Routes } from "react-router-dom";
-import UserContext from "../../components/context/UserContext";
 
 export default function Home() {
-  const [loginStatus, setLoginStatus] = useState(false);
   return (
     <div className="app-api">
-      <UserContext.Provider value={{ loginStatus, setLoginStatus }}>
-        <div className="app-api-container">
-          <Navbar />
-          <Routes>
-            <Route path="" element={<Documentation />} />
-            <Route path="register" element={<Register />} />
-            <Route path="login" element={<Login />} />
-            <Route path="account" element={<Account />} />
-          </Routes>
-        </div>
-      </UserContext.Provider>
+      <div className="app-api-container">
+        <Navbar />
+        <Routes>
+          <Route path="" element={<Documentation />} />
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+          <Route path="account" element={<Account />} />
+        </Routes>
+      </div>
     </div>
   );
 }
