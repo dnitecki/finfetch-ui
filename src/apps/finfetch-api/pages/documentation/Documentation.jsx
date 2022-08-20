3923ad4c3610e20d345cb72d8f000cd820c5abe8
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-19 19:48:18 -0500

    more docs styles

--- src/apps/finfetch-api/pages/documentation/Documentation.jsx
+++ src/apps/finfetch-api/pages/documentation/Documentation.jsx
@@ -23,16 +23,16 @@ export default function Documentation() {
         <div className="documentation-content-container">
           <div className="documentation-header"></div>
           <div className="documentation-content-container-scroll">
-            <div className="documentation-register-header">
-              FinFetch Stock API
-            </div>
             <div className="documentation-register">
-              <div className="documentation-register-text">
-                A truly democratized stock data RESTful API.
+              <div className="documentation-register-header">
+                FinFetch Stock API
               </div>
               <div className="documentation-register-text">
-                Access real-time financial data to power your projects for
-                <strong>&nbsp;FREE.</strong>
+                <div>A truly democratized stock data RESTful API.</div>
+                <div>
+                  Access real-time financial data to power your projects for
+                  <strong>&nbsp;FREE.</strong>
+                </div>
               </div>
               <div className="documentation-register-button">
                 <div id="register-button-container">
@@ -47,67 +47,69 @@ export default function Documentation() {
                 </div>
               </div>
             </div>
-            <div className="documentation-content-header">
-              API Documentation
-            </div>
-            <div className="documentation-content">
-              <div className="documentation-text">
-                <div className="documentation-content-header-text">
-                  Stock Price <div className="request-type-get">GET</div>
+            <div className="documentation-content-wrapper">
+              <div className="documentation-content-header">
+                API Documentation
+              </div>
+              <div className="documentation-content">
+                <div className="documentation-text">
+                  <div className="documentation-content-header-text">
+                    Stock Price <div className="request-type-get">GET</div>
+                  </div>
+                </div>
+                <div className="documentation-code">
+                  <div className="documentation-code-header">
+                    Sample JSON response
+                  </div>
+                  <SyntaxHighlighter
+                    useInlineStyles={true}
+                    className="syntax-highlighter"
+                    language="json"
+                    style={coy}
+                  >
+                    {stockPrice}
+                  </SyntaxHighlighter>
                 </div>
               </div>
-              <div className="documentation-code">
-                <div className="documentation-code-header">
-                  Sample JSON response
+              <div className="documentation-content">
+                <div className="documentation-text">
+                  <div className="documentation-content-header-text">
+                    Stock Info <div className="request-type-get">GET</div>
+                  </div>
                 </div>
-                <SyntaxHighlighter
-                  useInlineStyles={true}
-                  className="syntax-highlighter"
-                  language="json"
-                  style={coy}
-                >
-                  {stockPrice}
-                </SyntaxHighlighter>
-              </div>
-            </div>
-            <div className="documentation-content">
-              <div className="documentation-text">
-                <div className="documentation-content-header-text">
-                  Stock Info <div className="request-type-get">GET</div>
-                </div>
-              </div>
-              <div className="documentation-code">
-                <div className="documentation-code-header">
-                  Sample JSON response
-                </div>
-                <SyntaxHighlighter
-                  useInlineStyles={true}
-                  className="syntax-highlighter"
-                  language="json"
-                  style={coy}
-                >
-                  {stockInfo}
-                </SyntaxHighlighter>
-              </div>
-            </div>
-            <div className="documentation-content">
-              <div className="documentation-text">
-                <div className="documentation-content-header-text">
-                  Stock News <div className="request-type-get">GET</div>
+                <div className="documentation-code">
+                  <div className="documentation-code-header">
+                    Sample JSON response
+                  </div>
+                  <SyntaxHighlighter
+                    useInlineStyles={true}
+                    className="syntax-highlighter"
+                    language="json"
+                    style={coy}
+                  >
+                    {stockInfo}
+                  </SyntaxHighlighter>
                 </div>
               </div>
-              <div className="documentation-code">
-                <div className="documentation-code-header">
-                  Sample JSON response
+              <div className="documentation-content">
+                <div className="documentation-text">
+                  <div className="documentation-content-header-text">
+                    Stock News <div className="request-type-get">GET</div>
+                  </div>
                 </div>
-                <SyntaxHighlighter
-                  useInlineStyles={true}
-                  className="syntax-highlighter"
-                  language="json"
-                  style={coy}
-                >
-                  {stockNews}
-                </SyntaxHighlighter>
+                <div className="documentation-code">
+                  <div className="documentation-code-header">
+                    Sample JSON response
+                  </div>
+                  <SyntaxHighlighter
+                    useInlineStyles={true}
+                    className="syntax-highlighter"
+                    language="json"
+                    style={coy}
+                  >
+                    {stockNews}
+                  </SyntaxHighlighter>
+                </div>
               </div>
             </div>
           </div>
