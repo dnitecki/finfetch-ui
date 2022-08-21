9338accdbca681405ea28452da1d355f75d1d515
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-20 20:57:51 -0500

    add 404 page

--- src/apps/finfetch-api/Home.jsx
+++ src/apps/finfetch-api/Home.jsx
@@ -5,6 +5,7 @@ import Login from "./pages/login/Login";
 import Documentation from "./pages/documentation/Documentation";
 import Register from "./pages/register/Register";
 import Account from "./pages/account/Account";
+import PageNotFound from "../../pages/pageNotFound/PageNotFound";
 import { Route, Routes } from "react-router-dom";
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
           <Route path="register" element={<Register />} />
           <Route path="signin" element={<Login />} />
           <Route path="account" element={<Account />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </div>
     </div>
--- src/pages/pageNotFound/PageNotFound.jsx
+++ src/pages/pageNotFound/PageNotFound.jsx
@@ -1,6 +1,36 @@
 import React from "react";
-import "./PageNotFound";
+import "./PageNotFound.scss";
+import { Helmet } from "react-helmet";
+import { NavLink } from "react-router-dom";
 
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound">
+      <Helmet>
+        <title>FinFetch | Page Not Found</title>
+      </Helmet>
+      <div className="bg-animation">
+        <div className="bg-layer-3"></div>
+        <div className="bg-layer-4"></div>
+      </div>
+      <div className="pageNotFound-container">
+        <div className="pageNotFound-header">404 :(</div>
+        <div className="pageNotFound-subHeader">Oops, page not found.</div>
+        <div className="pageNotFound-text">
+          You've wandered off too far or I've broken something.
+        </div>
+        <div className="pageNotFound-text">
+          Either way, let's pretend this never happened...
+        </div>
+        <div className="pageNotFound-buttons">
+          <NavLink to="/">
+            <button className="pageNotFound-button">Home</button>
+          </NavLink>
+          <NavLink to="/api/docs">
+            <button className="pageNotFound-button">API Docs</button>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 }
