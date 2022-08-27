bbb30d18ab87c26a511f1844f4a0b22ea6c86b31
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-08-26 23:37:20 -0500

    change text

--- src/apps/finfetch-api/components/navbar/Navbar.jsx
+++ src/apps/finfetch-api/components/navbar/Navbar.jsx
@@ -76,7 +76,7 @@ export default function Navbar() {
         }
       >
         <FontAwesomeIcon icon={faCheck} />
-        <div className="success-text">Successfully logged out</div>
+        <div className="success-text">Logged out successfully</div>
       </div>
     </div>
   );
