bd8493d6476a0278d3bf66681898c3eb9a8b0439
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-03 21:53:32 -0500

    update registration form

--- src/apps/finfetch-api/pages/register/Register.jsx
+++ src/apps/finfetch-api/pages/register/Register.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Register.scss";
 import { Helmet } from "react-helmet";
-import RegistrationForm from "../../components/registration/RegistrationForm";
+import RegistrationForm from "../../components/registrationForm/RegistrationForm";
 
 export default function Register() {
   return (
-    <div className="register">
-      <Helmet>
-        <title>FinFetch | Register</title>
-      </Helmet>
-      <div className="bg-animation">
-        <div className="bg-layer-3"></div>
-        <div className="bg-layer-4"></div>
+    <>
+      <div className="register">
+        <Helmet>
+          <title>FinFetch | Register</title>
+        </Helmet>
+        <div className="bg-animation">
+          <div className="bg-layer-3"></div>
+          <div className="bg-layer-4"></div>
+        </div>
+        <RegistrationForm />
       </div>
-      <RegistrationForm />
-    </div>
+    </>
   );
 }
