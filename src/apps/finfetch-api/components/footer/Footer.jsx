f82837e61e8470724bae7c92f31df9e118566de3
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-04 23:31:01 -0500

    update footer

--- src/apps/finfetch-api/components/footer/Footer.jsx
+++ src/apps/finfetch-api/components/footer/Footer.jsx
@@ -10,9 +10,13 @@ export default function Footer() {
           <div className="footer-left">
             <div className="footer-logo-container">
               <img className="footer-logo" src={logo} alt="FinFetch.io"></img>
+              <div className="footer-text">
+                Copyright © 2022 FinFetch.io, Inc.
+              </div>
             </div>
-            <div className="footer-text">
-              Copyright © 2022 FinFetch.io, Inc.
+            <div className="footer-policies">
+              <div className="footer-text-links">Privacy Policy</div>
+              <div className="footer-text-links">Legal Stuff</div>
             </div>
           </div>
           <div className="footer-right"></div>
