38472735fe3e545c05e06742f3a03373bab1949d
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-04 23:12:02 -0500

    add footer

--- src/apps/finfetch-api/components/footer/Footer.jsx
+++ src/apps/finfetch-api/components/footer/Footer.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import "./Footer.scss";
+import logo from "../../../../assets/FinFetch-icon-text.png";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="footer-left">
+            <div className="footer-logo-container">
+              <img className="footer-logo" src={logo} alt="FinFetch.io"></img>
+            </div>
+            <div className="footer-text">
+              Copyright © 2022 FinFetch.io, Inc.
+            </div>
+          </div>
+          <div className="footer-right"></div>
+        </div>
+      </div>
+    </>
+  );
 }
