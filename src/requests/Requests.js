0734743ec0f62e22b864aafc7ef4b5fd1330c5f4
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-04 22:28:42 -0500

    small cleanup

--- src/requests/Requests.js
+++ src/requests/Requests.js
@@ -15,10 +15,10 @@ export const registerUser = async (email, pwd) => {
 export const loginUser = async (email, pwd) => {
   const url = `${basePath}/api/login/`;
   await axios.post(url, JSON.stringify({ email: email, password: pwd }), {
-    withCredentials: true,
     headers: {
       "Content-Type": "application/json",
     },
+    withCredentials: true,
   });
 };
 
