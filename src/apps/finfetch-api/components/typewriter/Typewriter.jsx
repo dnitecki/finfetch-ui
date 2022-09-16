91674d1cd0cdb41912dcabeea5458252435662ba
Dominck Nitecki <91350399+dnitecki@users.noreply.github.com>
2022-09-16 10:23:18 -0500

    commit

--- src/apps/finfetch-api/components/typewriter/Typewriter.jsx
+++ src/apps/finfetch-api/components/typewriter/Typewriter.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUp,
+  faFingerprint,
+  faRocket,
+  faCode,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function TypewriterEffect() {
   return (
     <>
       <Typewriter
         options={{
-          strings: ["Verified ", "Real-Time ", "Absolutely Free "],
+          strings: ["Verified ✓ ", "Real-Time ✓ ", "Absolutely Free ✓ "],
           autoStart: true,
           delay: 75,
           loop: true,
